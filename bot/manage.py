6e51eac6bcb60832af583ab0a3139d85b43a5370
yuda <maccabibibi@gmail.com>
2021-05-16 01:30:53 +0300

    db_session

--- bot/manage.py
+++ bot/manage.py
@@ -7,9 +7,10 @@ ids = []
 
 @Client.on_message(filters.command("get_static") & filters.chat(ids))
 def get_static(_, msg: types.Message):
-    users = select(user for user in User)[:]
-    users_list = [user.uid for user in users]
-    with open("users.json", "w") as users_file:
-        dump(users_list, users_file, indent=4)
+    with db_session:
+        users = select(user for user in User)[:]
+        users_list = [user.uid for user in users]
+        with open("users.json", "w") as users_file:
+            dump(users_list, users_file, indent=4)
 
-    msg.reply_document("users.json", caption=f"len of users: {len(users_list)}")
+        msg.reply_document("users.json", caption=f"len of users: {len(users_list)}")
